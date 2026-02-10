f7a9e419972b0cd33b30e168a0ac9983d9318132
赵志伟 <978964140@qq.com>
2026-02-10 17:53:49 +0800

    Revert "Revert "refactor(analytics-stat): 统一包名结构为 io.metersphere""
    This reverts commit 008c81eb76e7c7894bcf337eaf6a6a39d726312f.

--- analytics-stat/backend/src/main/java/io/metersphere/controller/HealthController.java
+++ analytics-stat/backend/src/main/java/io/metersphere/controller/HealthController.java
@@ -1,4 +1,4 @@
-package io.metersphere.analyticsstat.controller;
+package io.metersphere.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
