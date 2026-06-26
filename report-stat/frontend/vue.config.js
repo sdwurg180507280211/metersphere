620f6a27e798cc2e9d80ac0625758e13de0a05b2
赵志伟 <978964140@qq.com>
2026-06-26 20:01:08 +0800

    fix: disable dev overlay for frontend modules

--- report-stat/frontend/vue.config.js
+++ report-stat/frontend/vue.config.js
@@ -15,6 +15,7 @@ module.exports = defineConfig({
     port: 4006,
     client: {
       webSocketTransport: "sockjs",
+      overlay: false,
     },
     allowedHosts: "all",
     webSocketServer: "sockjs",
--- workstation/frontend/vue.config.js
+++ workstation/frontend/vue.config.js
@@ -15,6 +15,7 @@ module.exports = defineConfig({
     port: 4007,
     client: {
       webSocketTransport: "sockjs",
+      overlay: false,
     },
     allowedHosts: "all",
     webSocketServer: "sockjs",
